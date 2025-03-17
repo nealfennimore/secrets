a02a2475651631cbabe8cb08aa3347d0bbaea316
Neal Fennimore <hi@neal.codes>
2025-03-17 15:25:13 -0400

    Update to return CORS on errors

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -26,8 +26,12 @@ fn cors_headers() -> Cors {
         .with_methods(vec![Method::Options, Method::Post])
 }
 
+fn response() -> Result<ResponseBuilder> {
+    ResponseBuilder::new().with_cors(&cors_headers())
+}
+
 async fn handle_options(_req: Request) -> Result<Response> {
-    Response::empty()?.with_cors(&cors_headers())
+    Ok(response()?.empty())
 }
 
 const KV_NAMESPACE: &str = "secrets";
@@ -43,29 +47,27 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
         .post_async("/retrieve", |mut req, ctx| async move {
             let payload = match req.json::<SecretId>().await {
                 Ok(p) => p,
-                Err(e) => return Response::error(e.to_string(), 400),
+                Err(e) => return response()?.error(e.to_string(), 400),
             };
 
             let kv = ctx.kv(&KV_NAMESPACE)?;
             let bytes = kv.get(&payload.id.to_string()).bytes().await?;
             if bytes.is_none() {
-                return Response::error("Secret not found", 404);
+                return response()?.error("Secret not found", 404);
             }
             kv.delete(&payload.id.to_string()).await?;
             let secret: Secret = serde_json::from_slice(&bytes.unwrap())?;
-            Response::builder()
-                .with_cors(&cors_headers())?
-                .from_json(&secret)
+            response()?.from_json(&secret)
         })
         .options_async("/store", |req, _ctx| handle_options(req))
         .post_async("/store", |mut req, ctx| async move {
             let payload = match req.json::<Secret>().await {
                 Ok(p) => p,
-                Err(e) => return Response::error(e.to_string(), 400),
+                Err(e) => return response()?.error(e.to_string(), 400),
             };
             let validation = payload.validate().map_err(|e| e.to_string());
             if validation.is_err() {
-                return Response::error(validation.err().unwrap(), 400);
+                return response()?.error(validation.err().unwrap(), 400);
             }
 
             let kv = ctx.kv(&KV_NAMESPACE)?;
@@ -75,9 +77,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
                 .expiration_ttl(60 * 60 * 24)
                 .execute()
                 .await?;
-            Response::builder()
-                .with_cors(&cors_headers())?
-                .from_json(&SecretId { id })
+            response()?.from_json(&SecretId { id })
         })
         .run(req, env)
         .await
